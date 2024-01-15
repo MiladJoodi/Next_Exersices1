c5f232f7c0db7e99c123b2c7f652bf4e7efc24a0
Milad Joodi <winbeta.com@gmail.com>
2024-01-15 14:58:25 +0330

    cont

---
+++ 04/app/news/latest/page.jsx
@@ -0,0 +1,8 @@
+
+export default function LatestNews() {
+  return (
+    <main>
+        <h2>LatestNews</h2>
+    </main>
+  )
+}
---
+++ 04/app/tickets/page.jsx
@@ -0,0 +1,9 @@
+
+export default function Tickets() {
+    return (
+      <main>
+          <h2>Tickets</h2>
+      </main>
+    )
+  }
+  
