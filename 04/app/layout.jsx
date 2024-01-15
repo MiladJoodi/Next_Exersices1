f12fc32f96e8c634b29812a9581e257e5f2eb4dc
Milad Joodi <winbeta.com@gmail.com>
2024-01-15 20:41:56 +0330

    ♥

--- 04/app/layout.jsx
+++ 04/app/layout.jsx
@@ -1,8 +1,8 @@
-import { Inter } from 'next/font/google'
+import { Rubik } from 'next/font/google'
 import './globals.css'
-import Link from 'next/link'
+import Navbar from './components/Navbar'
 
-const inter = Inter({ subsets: ['latin'] })
+const rubik = Rubik({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -12,13 +12,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <nav>
-          <h2>This is navbar</h2>
-          <Link href="/">Dashboard</Link>
-          <br />
-          <Link href="/tickets">Tickets</Link>
-        </nav>
+      <body className={rubik.className}>
+        <Navbar />
         {children}
       </body>
     </html>
