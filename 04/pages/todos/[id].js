b7825d0ab40fb6c1a387ab823caf272007970521
Milad Joodi <winbeta.com@gmail.com>
2024-01-17 01:10:35 +0330

    zz

--- 04/pages/todos/[id].js
+++ 04/pages/todos/[id].js
@@ -1,38 +1,52 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-function Todo({ id, title, completed }) {
+function Todo({ todo: { id, title, completed } }) {
+  const [todo, setTodo] = useState({ id, title, completed });
 
-  const [todo, setTodo] = useState({id, title, completed})
+  console.log(id, title, completed);
 
-  const toggleCompleteHandler = async ()=>{
-    const res = await fetch(`http://localhost:4000/todos/${todo.id}`,{
-      method: 'PUT',
+  const toggleCompleteHandler = async () => {
+    const res = await fetch(`http://localhost:4000/todos/${todo.id}`, {
+      method: "PUT",
       headers: {
-        'Content-Type' : 'application/json'
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         title: todo.title,
-        completed : !todo.completed
-      })
-    })
+        completed: !todo.completed,
+      }),
+    });
+    const data = await res.json();
 
-
-    if(res.status === 200){
-      setTodo(prevTodo=> ({...prevTodo, completed: !prevTodo.completed}))
-      console.log('Todo compeleted successfully');
+    if (res.status === 200) {
+      // setTodo((prevTodo) => ({ ...prevTodo, completed: !prevTodo.completed }));
+      setTodo(data);
+      console.log("Todo Completed Successfully :))");
     }
+  };
 
-
-  } 
-  
   return (
-    <li key={id}>
-      <h2>{todo.id} . {todo.title}</h2>
-      <p>Complete: {todo.completed ? '✔️' : '❌'}</p>
+    <li>
+      <h2>
+        {todo.id}. {todo.title}
+      </h2>
+      <p>Complete: {todo.completed ? "✅" : "❌"}</p>
       <button onClick={toggleCompleteHandler}>Toggle Completed</button>
       <hr />
     </li>
-  )
+  );
 }
 
-export default Todo
+export async function getServerSideProps(context) {
+  const { params } = context;
+  const res = await fetch(`http://localhost:4000/todos/${params.id}`);
+  const data = await res.json();
+
+  return {
+    props: {
+      todo: data,
+    },
+  };
+}
+
+export default Todo;
--- 04/pages/todos/index.js
+++ 04/pages/todos/index.js
@@ -1,28 +1,33 @@
-import Link from 'next/link'
-import React, { useEffect, useState } from 'react'
-import Todo from "./[id]"
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import Todo from "./[id]";
+import useSWR from "swr";
+
+const todosFetcher = () =>
+  fetch("http://localhost:4000/todos").then((res) => res.json());
 
 function Todos() {
+  const { data, error } = useSWR("todos", todosFetcher);
 
-    const [todos, setTodos] = useState([])
+  if (error) {
+    return <h1>UnKnown Error !!</h1>;
+  }
 
-    useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/todos')
-        .then(res => res.json())
-        .then(data=> setTodos(data))
-    }, [])
-
-    return (
-        <div>
-        <ul>
-          {
-            todos.length ? todos.map((todo) => (
-                <Todo key={todo.id} {...todo} />
-              )) : <h1>Todos not found</h1>
-          }
-        </ul>
-      </div>
-    )
+  return (
+    <div>
+      <ul>
+        {data?.length
+          ? data.map((todo) => (
+              <li key={todo.id}>
+                <Link href={`/todos/${todo.id}`}>
+                  {todo.id}- {todo.title}
+                </Link>
+              </li>
+            ))
+          : null}
+      </ul>
+    </div>
+  );
 }
 
-export default Todos
+export default Todos;
