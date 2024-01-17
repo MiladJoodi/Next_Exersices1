2e767055dd319c7c0e40af49397c6a277eaa766b
Milad Joodi <winbeta.com@gmail.com>
2024-01-17 23:28:37 +0330

    Endure

---
+++ 05/components/modules/ServiceItem/ServiceItem.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function ServiceItem() {
+  return (
+    <div class="col-lg-6 mb-5">
+    <div class="row align-items-center">
+        <div class="col-sm-5">
+            <img class="img-fluid mb-3 mb-sm-0" src="images/service-4.jpg" alt="" />
+        </div>
+        <div class="col-sm-7">
+            <h4><i class="fa fa-table service-icon"></i>Online Table Booking</h4>
+            <p class="m-0">Sit lorem ipsum et diam elitr est dolor sed duo. Guberg sea et et lorem dolor sed est sit
+                invidunt, dolore tempor diam ipsum takima erat tempor</p>
+        </div>
+    </div>
+</div>
+  )
+}
+
+export default ServiceItem
