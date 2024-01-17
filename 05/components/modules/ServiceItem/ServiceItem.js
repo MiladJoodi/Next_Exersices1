75dea4a20f54c571c7e590308b8ca08198b3b523
Milad Joodi <winbeta.com@gmail.com>
2024-01-17 23:30:30 +0330

    e

--- 05/components/modules/ServiceItem/ServiceItem.js
+++ 05/components/modules/ServiceItem/ServiceItem.js
@@ -1,16 +1,15 @@
 import React from 'react'
 
-function ServiceItem() {
+function ServiceItem({title, desc, img, icon}) {
   return (
     <div class="col-lg-6 mb-5">
     <div class="row align-items-center">
         <div class="col-sm-5">
-            <img class="img-fluid mb-3 mb-sm-0" src="images/service-4.jpg" alt="" />
+            <img class="img-fluid mb-3 mb-sm-0" src={img} alt="" />
         </div>
         <div class="col-sm-7">
-            <h4><i class="fa fa-table service-icon"></i>Online Table Booking</h4>
-            <p class="m-0">Sit lorem ipsum et diam elitr est dolor sed duo. Guberg sea et et lorem dolor sed est sit
-                invidunt, dolore tempor diam ipsum takima erat tempor</p>
+            <h4><i class="fa fa-table service-icon"></i>{title}</h4>
+            <p class="m-0">{desc}</p>
         </div>
     </div>
 </div>
