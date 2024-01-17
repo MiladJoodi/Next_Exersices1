f9054b99148059ba874b858cbfd2e94df0944d8b
Milad Joodi <winbeta.com@gmail.com>
2024-01-17 22:06:23 +0330

    Front

--- 05/components/templates/Index/Slider.js
+++ 05/components/templates/Index/Slider.js
@@ -14,7 +14,16 @@ import { Navigation } from 'swiper/modules';
 function Slider() {
   return (
     <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>
+        <div class="carousel-item active">
+                    <img class="w-100" src="images/carousel-1.jpg" alt="Image" />
+                    <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                        <h2 class="text-primary font-weight-medium m-0">We Have Been Serving</h2>
+                        <h1 class="display-1 text-white m-0">COFFEE</h1>
+                        <h2 class="text-white m-0">* SINCE 1950 *</h2>
+                    </div>
+                </div>
+        </SwiperSlide>
         <SwiperSlide>Slide 2</SwiperSlide>
         <SwiperSlide>Slide 3 Test</SwiperSlide>
       </Swiper>
