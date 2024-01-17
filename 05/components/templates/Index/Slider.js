eedf0733c8997bb3e178e52103153706eecf08c3
Milad Joodi <winbeta.com@gmail.com>
2024-01-17 22:18:58 +0330

    swiperjs

--- 05/components/templates/Index/Slider.js
+++ 05/components/templates/Index/Slider.js
@@ -13,19 +13,23 @@ import { Navigation } from 'swiper/modules';
 
 function Slider() {
   return (
-    <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-        <SwiperSlide>
-        <div class="carousel-item active">
-                    <img class="w-100" src="images/carousel-1.jpg" alt="Image" />
-                    <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
+    <Swiper navigation={true} modules={[Navigation]} className={styles.swiper}>
+        <SwiperSlide className={styles.swiper_slide} style={{backgroundImage: 'url("/images/carousel-1.jpg")'}}>
+                    <div class={`${styles.slider_caption} d-flex flex-column align-items-center justify-content-center`}>
                         <h2 class="text-primary font-weight-medium m-0">We Have Been Serving</h2>
                         <h1 class="display-1 text-white m-0">COFFEE</h1>
                         <h2 class="text-white m-0">* SINCE 1950 *</h2>
                     </div>
-                </div>
         </SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3 Test</SwiperSlide>
+
+        <SwiperSlide className={styles.swiper_slide} style={{backgroundImage: 'url("/images/carousel-1.jpg")'}}>
+                    <div class={`${styles.slider_caption} d-flex flex-column align-items-center justify-content-center`}>
+                        <h2 class="text-primary font-weight-medium m-0">We Have Been Serving</h2>
+                        <h1 class="display-1 text-white m-0">COFFEE</h1>
+                        <h2 class="text-white m-0">* SINCE 1950 *</h2>
+                    </div>
+        </SwiperSlide>
+        
       </Swiper>
   )
 }
