4b680be117b8dce2978cce40cf2e4797117c4b4e
Milad Joodi <winbeta.com@gmail.com>
2024-01-17 23:52:05 +0330

    00

--- 05/components/templates/Index/Services.js
+++ 05/components/templates/Index/Services.js
@@ -1,66 +1,49 @@
+import ServiceItem from '@/components/modules/ServiceItem/ServiceItem'
 import React from 'react'
 
 function Services() {
-  return (
-    <div class="container-fluid pt-5">
-        <div class="container">
-            <div class="section-title">
-                <h4 class="text-primary text-uppercase" style={{letterSpacing: '5px'}}>Our Services</h4>
-                <h1 class="display-4">Fresh &amp; Organic Beans</h1>
-            </div>images/
-            <div class="row">
-                <div class="col-lg-6 mb-5">
-                    <div class="row align-items-center">
-                        <div class="col-sm-5">
-                            <img class="img-fluid mb-3 mb-sm-0" src="images/service-1.jpg" alt="" />
-                        </div>
-                        <div class="col-sm-7">
-                            <h4><i class="fa fa-truck service-icon"></i>Fastest Door Delivery</h4>
-                            <p class="m-0">Sit lorem ipsum et diam elitr est dolor sed duo. Guberg sea et et lorem dolor sed est sit
-                                invidunt, dolore tempor diam ipsum takima erat tempor</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6 mb-5">
-                    <div class="row align-items-center">
-                        <div class="col-sm-5">
-                            <img class="img-fluid mb-3 mb-sm-0" src="images/service-2.jpg" alt="" />
-                        </div>
-                        <div class="col-sm-7">
-                            <h4><i class="fa fa-coffee service-icon"></i>Fresh Coffee Beans</h4>
-                            <p class="m-0">Sit lorem ipsum et diam elitr est dolor sed duo. Guberg sea et et lorem dolor sed est sit
-                                invidunt, dolore tempor diam ipsum takima erat tempor</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6 mb-5">
-                    <div class="row align-items-center">
-                        <div class="col-sm-5">
-                            <img class="img-fluid mb-3 mb-sm-0" src="images/service-3.jpg" alt="" />
-                        </div>
-                        <div class="col-sm-7">
-                            <h4><i class="fa fa-award service-icon"></i>Best Quality Coffee</h4>
-                            <p class="m-0">Sit lorem ipsum et diam elitr est dolor sed duo. Guberg sea et et lorem dolor sed est sit
-                                invidunt, dolore tempor diam ipsum takima erat tempor</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6 mb-5">
-                    <div class="row align-items-center">
-                        <div class="col-sm-5">
-                            <img class="img-fluid mb-3 mb-sm-0" src="images/service-4.jpg" alt="" />
-                        </div>
-                        <div class="col-sm-7">
-                            <h4><i class="fa fa-table service-icon"></i>Online Table Booking</h4>
-                            <p class="m-0">Sit lorem ipsum et diam elitr est dolor sed duo. Guberg sea et et lorem dolor sed est sit
-                                invidunt, dolore tempor diam ipsum takima erat tempor</p>
-                        </div>
-                    </div>
+    return (
+        <div class="container-fluid pt-5">
+            <div class="container">
+                <div class="section-title">
+                    <h4 class="text-primary text-uppercase" style={{ letterSpacing: '5px' }}>Our Services</h4>
+                    <h1 class="display-4">Fresh &amp; Organic Beans</h1>
+                </div>images/
+                <div class="row">
+
+                    <ServiceItem
+                        title="Fastest Door Delivery"
+                        desc="Sit lorem ipsum et diam elitr est dolor sed duo. Guberg sea et et lorem dolor sed est sit
+                invidunt, dolore tempor diam ipsum takima erat tempor"
+                        img="images/service-1.jpg"
+                    />
+
+                    <ServiceItem
+                        title="Fresh Coffee Beans"
+                        desc="Sit lorem ipsum et diam elitr est dolor sed duo. Guberg sea et et lorem dolor sed est sit
+                invidunt, dolore tempor diam ipsum takima erat tempor"
+                        img="images/service-2.jpg"
+                    />
+
+                    <ServiceItem
+                        title="Best Quality Coffee"
+                        desc="Sit lorem ipsum et diam elitr est dolor sed duo. Guberg sea et et lorem dolor sed est sit
+                invidunt, dolore tempor diam ipsum takima erat tempor"
+                        img="images/service-3.jpg"
+                    />
+
+                    <ServiceItem
+                        title="Online Table Booking"
+                        desc="Sit lorem ipsum et diam elitr est dolor sed duo. Guberg sea et et lorem dolor sed est sit
+                invidunt, dolore tempor diam ipsum takima erat tempor"
+                        img="images/service-4.jpg"
+                    />
+
+
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Services
