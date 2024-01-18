bc2698cad6ad754047305819478f0ee2fb9f9773
Milad Joodi <winbeta.com@gmail.com>
2024-01-18 23:00:42 +0330

    0

--- 05/components/templates/Menu/Pricing.js
+++ 05/components/templates/Menu/Pricing.js
@@ -1,8 +1,34 @@
+import Card from '@/components/modules/Card/Card'
 import React from 'react'
 
-function Pricing() {
+function Pricing({data}) {
   return (
-    <div>Pricing</div>
+    <div className="container-fluid pt-5">
+        <div className="container">
+            <div className="section-title">
+                <h4 className="text-primary text-uppercase" style={{letterSpacing: '5px'}}>Menu &amp; Pricing</h4>
+                <h1 className="display-4">Competitive Pricing</h1>
+            </div>
+            <div className="row">
+                <div className="col-lg-6">
+                    <h1 className="mb-5">Hot Coffee</h1>
+                    {
+                      data.filter(item=> item.type === 'hot').slice(0,3).map(item=> (
+                        <Card {...item} key={item.key} />
+                      ))
+                    }
+                </div>
+                <div className="col-lg-6">
+                    <h1 className="mb-5">Cold Coffee</h1>
+                    {
+                      data.filter(item=> item.type === 'cold').slice(0,3).map(item=> (
+                        <Card {...item} key={item.key} />
+                      ))
+                    }
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
--- 05/pages/menu/index.js
+++ 05/pages/menu/index.js
@@ -1,10 +1,26 @@
 import PageHeader from '@/components/modules/PageHeader/PageHeader'
+import Pricing from '@/components/templates/Menu/Pricing'
 import React from 'react'
 
-function Menu() {
+function Menu({ menu }) {
   return (
-    <PageHeader route="Menu" />
+    <>
+      <PageHeader route="Menu" />
+      <Pricing data={menu} />
+    </>
   )
+}
+
+export async function getStaticProps(context) {
+
+  const res = await fetch('http://localhost:4000/menu')
+  const data = await res.json()
+
+  return {
+    props: {
+      menu: data,
+    }
+  }
 }
 
 export default Menu
