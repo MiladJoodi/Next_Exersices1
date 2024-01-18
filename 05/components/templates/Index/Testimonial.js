467dca62f95d01eb962aa009bf7dc2f3be43aa44
Milad Joodi <winbeta.com@gmail.com>
2024-01-18 19:47:35 +0330

    testomot

--- 05/components/templates/Index/Testimonial.js
+++ 05/components/templates/Index/Testimonial.js
@@ -2,7 +2,56 @@ import React from 'react'
 
 function Testimonial() {
   return (
-    <div>Testimonial</div>
+    <div class="container-fluid py-5">
+        <div class="container">
+            <div class="section-title">
+                <h4 class="text-primary text-uppercase" style="letter-spacing: 5px;">Testimonial</h4>
+                <h1 class="display-4">Our Clients Say</h1>
+            </div>
+            <div class="owl-carousel testimonial-carousel">
+                <div class="testimonial-item">
+                    <div class="d-flex align-items-center mb-3">
+                        <img class="img-fluid" src="/images/testimonial-1.jpg" alt="" />
+                        <div class="ml-3">
+                            <h4>Client Name</h4>
+                            <i>Profession</i>
+                        </div>
+                    </div>
+                    <p class="m-0">Sed ea amet kasd elitr stet, stet rebum et ipsum est duo elitr eirmod clita lorem. Dolor tempor ipsum sanct clita</p>
+                </div>
+                <div class="testimonial-item">
+                    <div class="d-flex align-items-center mb-3">
+                        <img class="img-fluid" src="/images/testimonial-2.jpg" alt="" />
+                        <div class="ml-3">
+                            <h4>Client Name</h4>
+                            <i>Profession</i>
+                        </div>
+                    </div>
+                    <p class="m-0">Sed ea amet kasd elitr stet, stet rebum et ipsum est duo elitr eirmod clita lorem. Dolor tempor ipsum sanct clita</p>
+                </div>
+                <div class="testimonial-item">
+                    <div class="d-flex align-items-center mb-3">
+                        <img class="img-fluid" src="/images/testimonial-3.jpg" alt="" />
+                        <div class="ml-3">
+                            <h4>Client Name</h4>
+                            <i>Profession</i>
+                        </div>
+                    </div>
+                    <p class="m-0">Sed ea amet kasd elitr stet, stet rebum et ipsum est duo elitr eirmod clita lorem. Dolor tempor ipsum sanct clita</p>
+                </div>
+                <div class="testimonial-item">
+                    <div class="d-flex align-items-center mb-3">
+                        <img class="img-fluid" src="/images/testimonial-4.jpg" alt="" />
+                        <div class="ml-3">
+                            <h4>Client Name</h4>
+                            <i>Profession</i>
+                        </div>
+                    </div>
+                    <p class="m-0">Sed ea amet kasd elitr stet, stet rebum et ipsum est duo elitr eirmod clita lorem. Dolor tempor ipsum sanct clita</p>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
--- 05/pages/index.js
+++ 05/pages/index.js
@@ -5,6 +5,7 @@ import Services from '@/components/templates/Index/Services'
 import Offer from '@/components/templates/Index/Offer'
 import Menu from '@/components/templates/Index/Menu'
 import Reservation from '@/components/templates/Index/Reservation'
+import Testimonial from '@/components/templates/Index/Testimonial'
 
 
 
@@ -17,6 +18,7 @@ function Index({data}) {
       <Offer />
       <Menu data={data.menu} />
       <Reservation />
+      <Testimonial />
     </>
   )
 }
@@ -29,12 +31,17 @@ export async function getStaticProps() {
   const menuResponse = await fetch("http://localhost:4000/menu")
   const menuData = await menuResponse.json()
 
+  const commentsResponse = await fetch("http://localhost:4000/comments")
+  const commentsData = await commentsResponse.json()
+
+
   
   return {
     props: {
       data: {
         services: servicesData,
         menu: menuData,
+        comments: commentsData,
       },
     },
     revalidate: 60 * 60 * 12
