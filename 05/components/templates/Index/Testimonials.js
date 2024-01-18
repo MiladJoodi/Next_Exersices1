5c27ce5a9ee71701ca6f84782d8f01afefa8428c
Milad Joodi <winbeta.com@gmail.com>
2024-01-18 20:20:07 +0330

    ee

--- 05/components/templates/Index/Testimonials.js
+++ 05/components/templates/Index/Testimonials.js
@@ -3,13 +3,13 @@ import React from 'react'
 
 function Testimonials({data}) {
   return (
-    <div class="container-fluid py-5">
-        <div class="container">
-            <div class="section-title">
-                <h4 class="text-primary text-uppercase" style={{letterSpacing: "5px"}}>Testimonial</h4>
-                <h1 class="display-4">Our Clients Say</h1>
+    <div className="container-fluid py-5">
+        <div className="container">
+            <div className="section-title">
+                <h4 className="text-primary text-uppercase" style={{letterSpacing: "5px"}}>Testimonial</h4>
+                <h1 className="display-4">Our Clients Say</h1>
             </div>
-            <div class="owl-carousel testimonial-carousel">
+            <div className="owl-carousel testimonial-carousel">
 
               {
                 data.slice(0,4).map(comment=> (
