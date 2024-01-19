da73dc731e53198b94e6dccc48e228f855879660
Milad Joodi <winbeta.com@gmail.com>
2024-01-19 20:50:08 +0330

    server side rendering

--- 05/pages/search/index.js
+++ 05/pages/search/index.js
@@ -5,27 +5,28 @@ import React from 'react'
 function Search() {
   return (
     <>
-    <PageHeader route="Search" />
-    <Result />
+      <PageHeader route="Search" />
+      <Result />
     </>
   )
 }
 
-export async function getServerSideProps(context){
+export async function getServerSideProps(context) {
 
-  const {query} = context;
-  
+  const { query } = context;
+
   const res = await fetch('http://localhost:4000/menu')
   const data = await res.json();
 
   const searchResult = data.filter(
-    item=> item.type.toLowerCase().includes(query.q.toLowerCase()) || item.title.toLowerCase().includes(query.q.toLowerCase())
-    );
+    item => item.type.toLowerCase().includes(query.q.toLowerCase()) ||
+    item.title.toLowerCase().includes(query.q.toLowerCase())
+  );
 
-  console.log('search result', item)
+  console.log('search result', searchResult)
 
-  return{
-    props:{
+  return {
+    props: {
 
     },
   }
