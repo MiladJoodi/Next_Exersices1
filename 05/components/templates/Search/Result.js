3ae34704cb09b480fc467d0827b5d39899092432
Milad Joodi <winbeta.com@gmail.com>
2024-01-19 20:22:14 +0330

    earch

---
+++ 05/components/templates/Search/Result.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Result() {
+  return (
+    <div className="container-fluid pt-5">
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-6">
+                    <h1 className="mb-5">Hot Coffee</h1>
+                    {/* {
+                      data.filter(item=> item.type === 'hot').slice(0,3).map(item=> (
+                        <Card {...item} key={item.key} />
+                      ))
+                    } */}
+                </div>
+                <div className="col-lg-6">
+                    <h1 className="mb-5">Cold Coffee</h1>
+                    {/* {
+                      data.filter(item=> item.type === 'cold').slice(0,3).map(item=> (
+                        <Card {...item} key={item.key} />
+                      ))
+                    } */}
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Result
--- 05/pages/search/index.js
+++ 05/pages/search/index.js
@@ -1,9 +1,32 @@
+import PageHeader from '@/components/modules/PageHeader/PageHeader'
+import Result from '@/components/templates/Search/Result'
 import React from 'react'
 
 function Search() {
   return (
-    <div>Search Page: </div>
+    <>
+    <PageHeader route="Search" />
+    <Result />
+    </>
   )
+}
+
+export async function getServerSideProps(context){
+
+  const {query} = context;
+  
+  const res = await fetch('http://localhost:4000/menu')
+  const data = await res.json();
+
+  const searchResult = data.filter(item=> (item.type.includes(query.q) || item.title.includes(query.q)))
+
+  console.log('search result', item)
+
+  return{
+    props:{
+
+    },
+  }
 }
 
 export default Search
