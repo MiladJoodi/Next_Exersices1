7fe7c2a29c7d95889efc09a01e6244588b007fc5
Milad Joodi <winbeta.com@gmail.com>
2024-01-19 23:04:27 +0330

    update n1

--- 05/components/templates/Product/ProductDetails.js
+++ 05/components/templates/Product/ProductDetails.js
@@ -19,11 +19,16 @@ const ProductsDetails = ({ data }) => {
         <h4 className="text-white mb-3">{data.title}</h4>
         <hr style={{ borderColor: "white" }} />
         <div className={styles.stars}>
-          <FontAwesomeIcon className={styles.fill_star} icon={faStar} />
-          <FontAwesomeIcon className={styles.fill_star} icon={faStar} />
-          <FontAwesomeIcon className={styles.fill_star} icon={faStar} />
-          <FontAwesomeIcon className={styles.fill_star} icon={faStar} />
-          <FontAwesomeIcon icon={faStar} />
+
+          {new Array(Math.trunc(data.score)).fill().map(() => (
+            <FontAwesomeIcon className={styles.fill_star} icon={faStar} />
+          ))}
+
+          {new Array(5 - Math.trunc(data.score)).fill().map(() => (
+            <FontAwesomeIcon icon={faStar} />
+          ))}
+
+
         </div>
         <div className={styles.price_details}>
 
