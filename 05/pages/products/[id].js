6cd669189e6d1637d0d158370e6d38e8e2ca747e
Milad Joodi <winbeta.com@gmail.com>
2024-01-19 22:20:39 +0330

    module

--- 05/pages/products/[id].js
+++ 05/pages/products/[id].js
@@ -2,13 +2,26 @@ import ProductsDetails from '@/components/templates/Product/ProductDetails'
 import Comments from '@/components/templates/Testmonial/Comments'
 import React from 'react'
 
-function Product() {
+function Product({product}) {
   return (
     <>
-      <ProductsDetails />
-      <Comments />
+      <ProductsDetails data={product} />
+      {/* <Comments /> */}
     </>
   )
+}
+
+export async function getStaticPaths(context){
+  const res = await fetch(`http://localhost:4000/menu`);
+  const products = await res.json()
+
+  const paths = products.map(product=> ({params: {id: String(product.id)}}))
+
+  return{
+    paths,
+    fallback: false,
+  }
+
 }
 
 export async function getStaticProps(context){
@@ -16,11 +29,11 @@ export async function getStaticProps(context){
 
   const res = await fetch(`http://localhost:4000/menu/${params.id}`);
   const product = await res.json()
-  console.log('product', product)
+  // console.log('product', product)
 
   return {
     props:{
-
+      product,
     }
   }
 }
