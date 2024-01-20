62fcb86fcc36dbe8bbf1f255863dffe512afa807
Milad Joodi <winbeta.com@gmail.com>
2024-01-20 23:50:17 +0330

    update

--- 06/pages/index.js
+++ 06/pages/index.js
@@ -1,26 +1,64 @@
 import { useState } from "react";
 
 function Homepage() {
-  const [users, setUsers] = useState([]);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const fetchUsers = async () => {
-    const res = await fetch("/api/users");
+  const registerUser = async (event) => {
+    event.preventDefault();
+
+    const newUser = {
+      username,
+      email,
+      password,
+    };
+
+    const res = await fetch(`/api/users`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newUser),
+    });
     const data = await res.json();
 
-    setUsers(data);
-    console.log("Users =>", data);
+    if (res.status === 201) {
+      setUsername("");
+      setEmail("");
+      setPassword("");
+    }
+    console.log("Response =>", res);
+    console.log("Response Data =>", data);
   };
 
   return (
     <>
-      <button onClick={fetchUsers}>Load Users</button>
-      <h3>Users:</h3>
-
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.username}</li>
-        ))}
-      </ul>
+      <div class="login-page">
+        <div class="form">
+          <form class="login-form">
+            <input
+              type="text"
+              value={username}
+              onChange={(event) => setUsername(event.target.value)}
+              placeholder="Username"
+            />
+            <input
+              type="text"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+              placeholder="Email"
+            />
+            <input
+              type="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+              placeholder="Password"
+            />
+            <button onClick={registerUser}>Register</button>
+          </form>
+        </div>
+      </div>
     </>
   );
 }
