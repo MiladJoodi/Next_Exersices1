b2338cda7d38f349dd9765979af4df4f706c89f8
Milad Joodi <winbeta.com@gmail.com>
2024-01-21 22:39:25 +0330

    0

--- 07.Mongodb/pages/api/index.js
+++ 07.Mongodb/pages/api/index.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const handler = (req, res) => {
   mongoose
     .connect("mongodb://localhost:27017/next-db")
-    .then(() => console.log("Connected!"));
+    .then(() => console.log("Connected!"))
+    .catch((err)=> console.log('Error => ', err))
 
   return res.json({ message: "Welcome to next.js api routes feature :))" });
 };
