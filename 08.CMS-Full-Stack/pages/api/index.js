961ade384794d916c7e1bd50789dbd61102c8f1f
Milad Joodi <winbeta.com@gmail.com>
2024-01-22 18:24:12 +0330

    10

---
+++ 08.CMS-Full-Stack/pages/api/index.js
@@ -0,0 +1,5 @@
+const handler = (req, res) => {
+    res.json({message: "wellcome"})
+}
+
+export default handler;
--- 08.CMS-Full-Stack/pages/index.js
+++ 08.CMS-Full-Stack/pages/index.js
@@ -1,7 +1,20 @@
 import Course from "@/components/templates/index/Course";
+import connectToDB from "@/utils/db";
 
 const index = () => {
   return <Course />;
 };
+
+export async function getStaticProps(context){
+
+  connectToDB()
+
+
+  return{
+    props:{
+
+    }
+  }
+}
 
 export default index;
