c24d23f6bbfa46f199373dacc8e1fbfa650a81fa
Milad Joodi <winbeta.com@gmail.com>
2024-01-23 23:01:21 +0330

    Update [id].js

--- 08.CMS-Full-Stack/pages/api/courses/[id].js
+++ 08.CMS-Full-Stack/pages/api/courses/[id].js
@@ -2,24 +2,40 @@ import connectToDB from "@/utils/db";
 import coursesModel from "@/models/course"
 import { isValidObjectId } from "mongoose";
 
-const handler = async (req, res)=>{
+const handler = async (req, res) => {
     connectToDB()
-    
-    if(req.method === 'DELETE'){
-        const {id} = req.query;
 
-    if(isValidObjectId(id)){
-        try{
-            await coursesModel.findOneAndDelete({_id : id})
-            return res.json({message: "Course Deleted"})
-        }catch(err) {
-            return res.status(500).json({message: "Internal Server Error"})
+    if (req.method === 'DELETE') {
+        const { id } = req.query;
+
+        if (isValidObjectId(id)) {
+            try {
+                await coursesModel.findOneAndDelete({ _id: id })
+                return res.json({ message: "Course Deleted" })
+            } catch (err) {
+                return res.status(500).json({ message: "Internal Server Error" })
+            }
+        } else {
+            return res.status(422).json({ message: "Course ID is not valid" })
         }
-    }else{
-        return res.status(422).json({message: "Course ID is not valid"})
+    } else if (req.method === "PUT") {
+        const { id } = req.query;
+        const { title } = req.body
+
+        if (isValidObjectId(id)) {
+            try {
+                await coursesModel.findOneAndUpdate(
+                    { _id: id },
+                    { title }
+                )
+            }catch(err){
+            return res.status(500).json({ message: "Internal server error" })
+            }
+        } else {
+            return res.status(422).json({ message: "Course ID is not valid" })
+        }
     }
-    }
-    
+
 
 }
 
