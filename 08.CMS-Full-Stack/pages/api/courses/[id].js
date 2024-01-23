233366ea785f5c26f25e8e430fa6777d9eda826c
Milad Joodi <winbeta.com@gmail.com>
2024-01-23 21:53:12 +0330

    0

---
+++ 08.CMS-Full-Stack/pages/api/courses/[id].js
@@ -0,0 +1,24 @@
+import connectToDB from "@/utils/db";
+import coursesModel from "@/models/course"
+import { isValidObjectId } from "mongoose";
+
+const handler = async (req, res)=>{
+    connectToDB()
+    const {id} = req.query;
+
+    if(isValidObjectId(id)){
+        try{
+            await coursesModel.findOneAndDelete({_id : id})
+            return res.json({message: "Course Deleted"})
+        }catch(err) {
+            return res.status(500).json({message: "Internal Server Error"})
+        }
+    }else{
+        return res.status(422).json(message: "Course ID is not valid")
+    }
+
+    
+
+}
+
+export default handler
