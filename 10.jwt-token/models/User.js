06d9b422f02cc83268b1a93ece2dd1a862969a2f
Milad Joodi <winbeta.com@gmail.com>
2024-01-26 20:44:10 +0330

    0

---
+++ 10.jwt-token/models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose")
+
+const schema = mongoose.Schema({
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        required: true
+    },
+})
