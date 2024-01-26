35070ca56d3f29a7a28912f245b1bfe00881b577
Milad Joodi <winbeta.com@gmail.com>
2024-01-26 15:54:58 +0330

    0

--- 09.CMS-Full-Stack_2/pages/search/index.js
+++ 09.CMS-Full-Stack_2/pages/search/index.js
@@ -10,17 +10,18 @@ export async function getServerSideProps(context){
   const data = await res.json()
   
   const {query} = context;
-  console.log(query)
+  const {q} = query
+//   console.log(query)
   
-  const posts = await data.find({title: {$regex: query.q}});
-  
-
-
-
+//   const filteredData = await data.find({title: {$regex: query.q}});
+const newData = await data.filter(function(post){
+    return post.title === q
+  })
+  console.log(newData)
 
   return{
     props:{
-    //   posts: JSON.parse(JSON.stringify(posts))
+      posts: JSON.parse(JSON.stringify(newData)),
     },
   }
 }
