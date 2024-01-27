9935bf24934e4330af536fe5b5f7c81342b7feb5
Milad Joodi <winbeta.com@gmail.com>
2024-01-27 23:34:15 +0330

    0

--- 10.jwt-token/pages/p-admin/index.js
+++ 10.jwt-token/pages/p-admin/index.js
@@ -1,7 +1,60 @@
+import connectToDB from '@/configs/db';
+import UserModel from '@/models/User'
+import { verifyToken } from "@/utils/auth";
 import React from "react";
 
-function PAdmin() {
-  return <h1>Welcome To Admin Panel ❤️</h1>;
+function PAdmin({user}) {
+  return <h1>Welcome To Admin Panel, {user.firstname} {user.lastname} ❤️</h1>;
+}
+
+// SSR
+export async function getServerSideProps(context) {
+  connectToDB()
+  const { token } = context.req.cookies
+
+  if (!token) {
+    return {
+      redirect: {
+        destination: '/signin'
+      }
+    }
+  }
+
+  const tokenPayload = verifyToken(token)
+
+  if (!tokenPayload) {
+    return {
+      redirect: {
+        detitaion: "/signin"
+      }
+    }
+  }
+
+  const user = await UserModel.findOne({
+    email: tokenPayload.email
+  },
+    '-_id firstname lastname role')
+
+    if(user.role !== "ADMIN"){
+      return{
+        redirect:{
+          destination: "/dashboard"
+        }
+      }
+    }
+    return{
+      props:{
+        user:JSON.parse(JSON.stringify(user))
+      }
+    }
+
+    console.log("user Role=> ", user.role)
+
+  return {
+    props: {
+      user: JSON.parse(JSON.stringify(user))
+    }
+  }
 }
 
 export default PAdmin;
