62fa127d4da3bbc3b2b71ad6fd1f5fcc4d095a53
Milad Joodi <winbeta.com@gmail.com>
2024-01-27 19:07:23 +0330

    0

--- 10.jwt-token/pages/dashboard/index.js
+++ 10.jwt-token/pages/dashboard/index.js
@@ -1,16 +1,18 @@
+import connectToDB from '@/configs/db';
 import UserModel from '@/models/User'
 import { verifyToken } from "@/utils/auth";
 import React from "react";
 
-function Dashboard() {
+function Dashboard({user}) {
   return (
     <>
-      <h1>Amin - Saeedi - Welcome To Dashboard</h1>
+      <h1>{user.firstname} - Welcome To Dashboard</h1>
     </>
   );
 }
 
 export async function getServerSideProps(context) {
+  connectToDB()
   // console.log(context.req.cookies)
   const { token } = context.req.cookies
 
@@ -24,24 +26,26 @@ export async function getServerSideProps(context) {
 
   const tokenPayload = verifyToken(token)
 
-  if (!tokenPayload){
-    return{
-      redirect:{
+  if (!tokenPayload) {
+    return {
+      redirect: {
         detitaion: "/signin"
       }
     }
   }
 
   const user = await UserModel.findOne({
-    email: tokenPayload.email })
+    email: tokenPayload.email
+  },
+    '-_id firstname lastname')
 
+    console.log(user)
 
-
-    return {
-      props: {
-
-      }
+  return {
+    props: {
+      user: JSON.parse(JSON.stringify(user))
     }
+  }
 }
 
 export default Dashboard;
