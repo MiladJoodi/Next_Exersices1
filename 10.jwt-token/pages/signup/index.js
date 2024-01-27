c2eac40dd6721a7e9390c9bee7f8c003a3eb96a4
Milad Joodi <winbeta.com@gmail.com>
2024-01-27 03:36:22 +0330

    0

--- 10.jwt-token/pages/signup/index.js
+++ 10.jwt-token/pages/signup/index.js
@@ -1,28 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Index() {
+  
+  const [firstname, setFirstname] = useState("")
+  const [lastname, setLastname] = useState("")
+  const [username, setsername] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
   return (
     <div className="box">
       <h1 align="center">SignUp Form</h1>
       <form role="form" method="post">
         <div className="inputBox">
-          <input type="text" autoComplete="off" required />
+          <input type="text" 
+          value={firstname}
+          autoComplete="off" required />
           <label>Firstname</label>
         </div>
         <div className="inputBox">
-          <input type="text" autoComplete="off" required />
+          <input type="text" 
+          value={lastname}
+          autoComplete="off" required />
           <label>Lastname</label>
         </div>
         <div className="inputBox">
-          <input type="text" autoComplete="off" required />
+          <input type="text" 
+          value={username}
+          autoComplete="off" required />
           <label>Username</label>
         </div>
         <div className="inputBox">
-          <input type="email" autoComplete="off" required />
+          <input type="email" 
+          value={email}
+          autoComplete="off" required />
           <label>Email</label>
         </div>
         <div className="inputBox">
-          <input type="password" autoComplete="off" required />
+          <input type="password" 
+          value={password}
+          onChange={(e)=>setPassword(e.target.value)}
+          autoComplete="off" required />
           <label>Password</label>
         </div>
 
