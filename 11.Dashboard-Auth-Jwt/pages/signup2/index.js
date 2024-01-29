6bde530743bce21152aa07e1e930c92a4d86f661
Milad Joodi <winbeta.com@gmail.com>
2024-01-30 00:22:51 +0330

    a

---
+++ 11.Dashboard-Auth-Jwt/pages/signup2/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Signup from '@/components/templates/signup/SignupX'
+
+function Signup() {
+    return (
+        <>
+        <Signup />
+        </>
+
+    )
+}
+
+export default Signup
