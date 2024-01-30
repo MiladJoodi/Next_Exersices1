071f4339c80282dcbf4e128869d53f537b7cbe74
Milad Joodi <winbeta.com@gmail.com>
2024-01-30 17:35:58 +0330

    0

---
+++ 11.Dashboard-Auth-Jwt/configs/db.js
@@ -0,0 +1,16 @@
+const { default: mongoose } = require("mongoose");
+
+const connectToDB = async () => {
+  try {
+    if (mongoose.connections[0].readyState) {
+      return true;
+    } else {
+      await mongoose.connect("mongodb://127.0.0.1:27017/next-auth-two");
+      console.log("Connect To DB Successfully :))");
+    }
+  } catch (err) {
+    console.log("DB Connection Has Error =>", err);
+  }
+};
+
+export default connectToDB;
--- 11.Dashboard-Auth-Jwt/pages/signup/index.js
+++ 11.Dashboard-Auth-Jwt/pages/signup/index.js
@@ -1,18 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "@/styles/signup.module.css";
 import Coffee from '@/components/modules/Coffee/Coffee';
 
-function Logout() {
+function Signup() {
+
+  // get inputs state
+  const [firstname, setFirstname] = useState("")
+  const [lastname, setLastname] = useState("")
+  const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+
   return (
     <div className={styles.wrapperCoffee}>
         <div className={styles.container}>
       <div className={styles.container__content}>
           <form className={styles.container__form}>
-            <input type='text' name='username' placeholder='نام' className={styles.container__inputField} />
-            <input type='text' name='username' placeholder='نام خانوادگی' className={styles.container__inputField} />
-            <input type='text' name='username' placeholder='نام کاربری' className={styles.container__inputField} />
-            <input type='text' name='username' placeholder='ایمیل' className={styles.container__inputField} />
-            <input type='password' name='password' placeholder='رمز عبور' className={styles.container__inputField} />
+            <input type='text' name='username' onChange={e=>setFirstname(e.target.value)} value={firstname} placeholder='نام' className={styles.container__inputField} />
+            <input type='text' name='username' onChange={e=>setLastname(e.target.value)} value={lastname} placeholder='نام خانوادگی' className={styles.container__inputField} />
+            <input type='text' name='username' onChange={e=>setUsername(e.target.value)} value={username} placeholder='نام کاربری' className={styles.container__inputField} />
+            <input type='text' name='username' onChange={e=>setEmail(e.target.value)} value={email} placeholder='ایمیل' className={styles.container__inputField} />
+            <input type='password' name='password' onChange={e=>setPassword(e.target.value)} value={password} placeholder='رمز عبور' className={styles.container__inputField} />
             <input type='submit' value='عضویت' className={`${styles.container__sumbitButton} mt-5`} />
           </form>
           <div className={styles.container__links}>
@@ -31,4 +40,4 @@ function Logout() {
   )
 }
 
-export default Logout
+export default Signup
