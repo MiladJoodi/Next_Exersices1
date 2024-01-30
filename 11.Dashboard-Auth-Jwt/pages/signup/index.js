74a717a2df6828c58c9088908616fabc583ddea1
Milad Joodi <winbeta.com@gmail.com>
2024-01-31 00:26:25 +0330

    0

--- 11.Dashboard-Auth-Jwt/pages/signup/index.js
+++ 11.Dashboard-Auth-Jwt/pages/signup/index.js
@@ -8,10 +8,29 @@ import toast, { Toaster } from "react-hot-toast";
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+import Link from "next/link";
+
+const SignupSchema = yup.object().shape({
+  firstname: yup.string().required("الزامی"),
+  lastname: yup.string().required("نام خانوادگی الزامیست"),
+  username: yup.string().min(3, "حداقل 3 کاراکتر").max(15, "حداکثر 15 کاراکتر").required("لطفاً نام کاربری را وارد کنید"),
+  email: yup.string().email('لطفا ایمیل را صحیح وارد کنید').min(3, "حداقل 3 کاراکتر").max(32, "حداکثر 32 کاراکتر").required(),
+  password: yup.string().min(8, "حداقل 8 کاراکتر").max(32, "حداکثر 32 کاراکتر").required(),
+  // confirmPassword: yup.string().oneOf([yup.ref("password")]).required()
+});
 
 function Signup() {
   // router
   const router = useRouter();
+
+  //Form
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(SignupSchema),
+  });
 
   // get inputs state
   const [firstname, setFirstname] = useState("");
@@ -20,70 +39,48 @@ function Signup() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  // Submit Method
-  const signupHandler = async (event) => {
-    event.preventDefault();
+  // const onSubmit = (data) => {
+  //   console.log(data)
+  // };
 
-    const user = {
-      firstname,
-      lastname,
-      username,
-      email,
-      password,
+    // Submit Method
+    const signupHandler = async (event) => {
+      // event.preventDefault();
+  
+      const user = {
+        firstname,
+        lastname,
+        username,
+        email,
+        password,
+      };
+  
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      });
+  
+      // 201
+      if (res.status === 201) {
+        toast.success("عضویت موفقیت آمیز بود");
+  
+        setTimeout(() => {
+          setFirstname("");
+          setLastname("");
+          setUsername("");
+          setEmail("");
+          setPassword("");
+          router.replace("dashboard");
+        }, 2000);
+      }
+      //422
+      if (res.status === 422) {
+        toast.error("لطفاً اطلاعات را کامل وارد کنید");
+      }
     };
-
-    const res = await fetch("/api/auth/signup", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(user),
-    });
-
-    // 201
-    if (res.status === 201) {
-      toast.success("عضویت موفقیت آمیز بود");
-
-      setTimeout(() => {
-        setFirstname("");
-        setLastname("");
-        setUsername("");
-        setEmail("");
-        setPassword("");
-        router.replace("dashboard");
-      }, 2000);
-    }
-    //422
-    if (res.status === 422) {
-      toast.error("لطفاً اطلاعات را کامل وارد کنید");
-    }
-  };
-
-  // React Hook Form
-
-  // const schema = yup.object().shape({
-  //   firstname: yup.string().min(3).max(15).required("وارد کردن نام الزامیست"),
-  //   lastname: yup.string().min(3).max(15).required(),
-  //   username: yup.string().min(3).max(15).required(),
-  //   email: yup.string().email().min(3).max(32).required(),
-  //   password: yup.string().min(4).max(15).required(),
-  //   // confirmPassword: yup.string().oneOf([yup.ref("password")]).required()
-  // })
-
-  const {
-    register,
-    control,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    defaultValues: {
-      name: "",
-    },
-  });
-
-  const formSubmiting = (data) => {
-    console.log("Data=> ", data);
-  };
 
   return (
     <div className={styles.wrapperCoffee}>
@@ -91,99 +88,95 @@ function Signup() {
         <div className={styles.container__content}>
           <form
             className={styles.container__form}
-            onSubmit={handleSubmit(formSubmiting)}
+            onSubmit={handleSubmit(signupHandler)}
           >
-            {/* <input
-              type="text"
-              name="firstname"
-              onChange={(e) => setFirstname(e.target.value)}
-              value={firstname}
-              placeholder="نام"
-              className={styles.container__inputField}
-              {...register("name", {
-                required: "وارد کردن نام الزامیست",
-                minLength: {
-                  value: 3,
-                  message: "حداقل 3 کاراکتر وارد کنید"
-                },
-                maxLength: {
-                  value: 12,
-                  message: "حداکثر 12 کاراکتر وارد کنید"
-                },
-              })}
-            />
-            {errors.name && errors.name.message} */}
+            <div>
+              <input
+                type="text"
+                {...register("firstname")}
+                onChange={(e) => setFirstname(e.target.value)}
+                value={firstname}
+                placeholder="نام"
+                className={styles.container__inputField}
+              />
+              {errors.firstname && (
+                <p className={styles.inputError}>{errors.firstname.message}</p>
+              )}
+            </div>
 
-            <input
-              type="text"
-              name="firstname"
-              {...register("firstname", {
-                required: "الزمایست",
-                minLength: {
-                  value: 1,
-                  message: "حداقل 3 کاراکتر",
-                },
-                maxLength: 12,
-              })}
-              onChange={(e) => setFirstname(e.target.value)}
-              value={firstname}
-              placeholder="نام"
-              className={styles.container__inputField}
-            />
-            {errors.firstname && errors.firstname.message}
+            <div>
+              <input
+                type="text"
+                {...register("lastname")}
+                onChange={(e) => setLastname(e.target.value)}
+                value={lastname}
+                placeholder="نام خانوادگی"
+                className={styles.container__inputField}
+              />
+              {errors.lastname && (
+                <p className={styles.inputError}>{errors.lastname.message}</p>
+              )}
+            </div>
 
-            <input
-              type="text"
-              name="lastname"
-              onChange={(e) => setUsername(e.target.value)}
-              value={username}
-              placeholder="نام کاربری"
-              className={styles.container__inputField}
-              // {...register("username")}
-            />
+            <div>
+              <input
+                type="text"
+                {...register("username")}
+                onChange={(e) => setUsername(e.target.value)}
+                value={username}
+                placeholder="نام کاربری"
+                className={styles.container__inputField}
+              />
+              {errors.username && (
+                <p className={styles.inputError}>{errors.username.message}</p>
+              )}
+            </div>
 
-            <input
-              type="text"
-              name="username"
-              onChange={(e) => setUsername(e.target.value)}
-              value={username}
-              placeholder="نام کاربری"
-              className={styles.container__inputField}
-              // {...register("username")}
-            />
-            <input
-              type="text"
-              name="email"
-              onChange={(e) => setEmail(e.target.value)}
-              value={email}
-              placeholder="ایمیل"
-              className={styles.container__inputField}
-              // {...register("email")}
-            />
-            <input
-              type="password"
-              name="password"
-              onChange={(e) => setPassword(e.target.value)}
-              value={password}
-              placeholder="رمز عبور"
-              className={styles.container__inputField}
-              // {...register("password")}
-            />
+            <div>
+              <input
+                type="text"
+                {...register("email")}
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                placeholder="ایمیل"
+                className={styles.container__inputField}
+              />
+              {errors.email && (
+                <p className={styles.inputError}>{errors.email.message}</p>
+              )}
+            </div>
+
+            <div>
+              <input
+                type="text"
+                {...register("password")}
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                placeholder="رمز عبور"
+                className={styles.container__inputField}
+              />
+              {errors.password && (
+                <p className={styles.inputError}>{errors.password.message}</p>
+              )}
+            </div>
+
+            {/* <input type="submit" /> */}
             <input
               type="submit"
               value="عضویت"
               className={`${styles.container__sumbitButton} `}
-              onClick={signupHandler}
+              // onClick={signupHandler}
             />
+            
           </form>
           <div className={styles.container__links}>
             <a href="#emptyLink" className={styles.container__link}>
               بازیابی رمز
             </a>
             <span className={styles.container__separator}></span>
-            <a href="#emptyLink" className={styles.container__link}>
-              ورود
-            </a>
+            <Link href="/signin" className={styles.container__link}>
+            ورود
+            </Link>
           </div>
         </div>
         <div className={styles.container__svgs}>
