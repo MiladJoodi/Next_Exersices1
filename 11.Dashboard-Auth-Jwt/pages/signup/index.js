3817afb57b9f60c594ef647a07a16b8c765ef869
Milad Joodi <winbeta.com@gmail.com>
2024-01-30 19:03:27 +0330

    00

--- 11.Dashboard-Auth-Jwt/pages/signup/index.js
+++ 11.Dashboard-Auth-Jwt/pages/signup/index.js
@@ -1,8 +1,13 @@
 import React, { useState } from 'react'
 import styles from "@/styles/signup.module.css";
 import Coffee from '@/components/modules/Coffee/Coffee';
+import { useRouter } from 'next/router';
+
 
 function Signup() {
+
+  // router
+  const router = useRouter()
 
   // get inputs state
   const [firstname, setFirstname] = useState("")
@@ -11,18 +16,60 @@ function Signup() {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
 
+  // Submit Method
+  const signup = async (event) => {
+    event.preventDefault()
+
+    const user = {
+      firstname,
+      lastname,
+      username,
+      email,
+      password
+    }
+
+    const res = await fetch("/api/auth/signup", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+
+    if (res.status === 201) {
+      setFirstname("")
+      setLastname("")
+      setUsername("")
+      setEmail("")
+      setPassword("")
+
+    // alert("submitted")
+    swal("تبریک میگم", "success");;
+
+
+    router.replace("dashboard")
+    }
+    if(res.status === 422){
+      
+
+    }
+    
+
+  }
+
+
 
   return (
-    <div className={styles.wrapperCoffee}>
-        <div className={styles.container}>
-      <div className={styles.container__content}>
-          <form className={styles.container__form}>
-            <input type='text' name='username' onChange={e=>setFirstname(e.target.value)} value={firstname} placeholder='نام' className={styles.container__inputField} />
-            <input type='text' name='username' onChange={e=>setLastname(e.target.value)} value={lastname} placeholder='نام خانوادگی' className={styles.container__inputField} />
-            <input type='text' name='username' onChange={e=>setUsername(e.target.value)} value={username} placeholder='نام کاربری' className={styles.container__inputField} />
-            <input type='text' name='username' onChange={e=>setEmail(e.target.value)} value={email} placeholder='ایمیل' className={styles.container__inputField} />
-            <input type='password' name='password' onChange={e=>setPassword(e.target.value)} value={password} placeholder='رمز عبور' className={styles.container__inputField} />
-            <input type='submit' value='عضویت' className={`${styles.container__sumbitButton} mt-5`} />
+    <div className={styles.wrapperCoffee} style={{backgroundImage: "@/images/Background.jpg"}}>
+      <div className={styles.container}>
+        <div className={styles.container__content}>
+          <form className={styles.container__form} onSubmit={signup}>
+            <input type='text' name='username' onChange={e => setFirstname(e.target.value)} value={firstname} placeholder='نام' className={styles.container__inputField} />
+            <input type='text' name='username' onChange={e => setLastname(e.target.value)} value={lastname} placeholder='نام خانوادگی' className={styles.container__inputField} />
+            <input type='text' name='username' onChange={e => setUsername(e.target.value)} value={username} placeholder='نام کاربری' className={styles.container__inputField} />
+            <input type='text' name='username' onChange={e => setEmail(e.target.value)} value={email} placeholder='ایمیل' className={styles.container__inputField} />
+            <input type='password' name='password' onChange={e => setPassword(e.target.value)} value={password} placeholder='رمز عبور' className={styles.container__inputField} />
+            <input type='submit' value='عضویت' className={`${styles.container__sumbitButton} `} />
           </form>
           <div className={styles.container__links}>
             <a href='#emptyLink' className={styles.container__link}>بازیابی رمز</a>
@@ -33,10 +80,9 @@ function Signup() {
         <div className={styles.container__svgs}>
           <Coffee />
         </div>
-
       </div>
-        </div>
-      
+    </div>
+
   )
 }
 
