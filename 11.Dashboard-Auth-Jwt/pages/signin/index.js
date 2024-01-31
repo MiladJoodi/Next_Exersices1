398cea4769a5719728dbc2d8ec039f49883a90cb
Milad Joodi <winbeta.com@gmail.com>
2024-01-31 19:31:21 +0330

    token jawf

--- 11.Dashboard-Auth-Jwt/pages/signin/index.js
+++ 11.Dashboard-Auth-Jwt/pages/signin/index.js
@@ -1,19 +1,37 @@
 import React, { useState } from 'react'
-import styles from "@/styles/signup.module.css";
+import styles from "@/styles/signin.module.css";
 import Coffee from '@/components/modules/Coffee/Coffee';
 import toast, { Toaster } from "react-hot-toast";
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 
-function Login() {
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const SigninSchema = yup.object().shape({
+  identifier: yup.string().required("نام کاربری الزامیست"),
+  password: yup.string().required("رمز عبور الزامیست"),
+});
+
+function Signin() {
+
+  //Form
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(SigninSchema),
+  });
+
 
   const router = useRouter()
 
   const [identifier, setIdentifier] = useState("")
   const [password, setPassword] = useState("")
 
-  const signin = async (event) => {
-    event.preventDefault()
-
+  const signinHandler = async (event) => {
     const user = { identifier, password }
 
     const res = await fetch("api/auth/signin", {
@@ -27,7 +45,7 @@ function Login() {
       // setIdentifier("")
       // setPassword("")
       
-      toast.success("عضویت موفقیت آمیز بود");
+      toast.success("ورود موفقیت آمیز بود");
 
       setTimeout(() => {
         router.replace("/dashboard")
@@ -41,21 +59,59 @@ function Login() {
       alert("Error")
     }
 
+
+
   }
+
 
   return (
     <div className={styles.wrapperCoffee}>
       <div className={styles.container}>
         <div className={styles.container__content}>
-          <form className={styles.container__form}>
-            <input type='text' name='username' value={identifier} onChange={(e) => setIdentifier(e.target.value)} placeholder='نام کاربری یا ایمیل' className={styles.container__inputField} />
-            <input type='password' name='password' value={password} onChange={(e) => setPassword(e.target.value)} placeholder='رمز عبور' className={styles.container__inputField} />
-            <input type='submit' value='ورود' onClick={signin} className={styles.container__sumbitButton} />
+          <form className={styles.container__form} onSubmit={handleSubmit(signinHandler)}>
+            <div>
+              <input
+                {...register("identifier")}
+                type='text'
+                name='identifier'
+                value={identifier}
+                onChange={(e) => setIdentifier(e.target.value)}
+                placeholder='نام کاربری یا ایمیل'
+                className={styles.container__inputField}
+              />
+              {errors.identifier && (
+                <p className={styles.inputError}>{errors.identifier.message}</p>
+              )}
+            </div>
+
+            <div>
+              <input
+                {...register("password")}
+                type='password'
+                name='password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder='رمز عبور'
+                className={styles.container__inputField}
+              />
+              {errors.password && (
+                <p className={styles.inputError}>{errors.password.message}</p>
+              )}
+            </div>
+
+            <input
+              type='submit' value='ورود'
+              className={styles.container__sumbitButton}
+            />
+
           </form>
+
           <div className={styles.container__links}>
             <a href='#emptyLink' className={styles.container__link}>بازیابی رمز</a>
             <span className={styles.container__separator}></span>
-            <a href='#emptyLink' className={styles.container__link}>عضویت</a>
+            <Link href="/signup" className={styles.container__link}>
+              عضویت
+            </Link>
           </div>
         </div>
         <div className={styles.container__svgs}>
@@ -70,4 +126,4 @@ function Login() {
   )
 }
 
-export default Login
+export default Signin
