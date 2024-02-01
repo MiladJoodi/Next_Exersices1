1bf47f45ba67a966622f1c5cde2ee37fc6d5b603
Milad Joodi <winbeta.com@gmail.com>
2024-02-01 15:26:44 +0330

    0

--- 12/pages/api/posts.js
+++ 12/pages/api/posts.js
@@ -1,20 +1,31 @@
-import { PrismaClient } from "@prisma/client"
-
+import {PrismaClient} from '@prisma/client';
 const prisma = new PrismaClient();
 
 export default async function userServices(req, res) {
 
+    // if (req.method === 'POST') {
+    //     const { title, content, authorId, published } = req.body
+    //     const user = await prisma.user.create({
+    //         data: {
+    //             email: "sss@gmail.com",
+    //             name: "miald",
+    //             password: "123456"
+    //         }
+    //     })
+    //     return res.json(user)
+    // }
+
     if (req.method === 'POST') {
-        const { title, content, authorId, published } = req.body
-        const post = await prisma.create({
-            data: {
-                title,
-                content,
-                authorId,
-                published,
-            }
-        })
-        return res.json(post)
-    }
+        const {title, content, authorId, published} = req.body;
+        const post = await prisma.post.create({
+          data: {
+            title,
+            content,
+            authorId,
+            published,
+          },
+        });
+        return res.json(post);
+      }
 
 }
