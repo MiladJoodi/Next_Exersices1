6a432d7ce2605221b519604022ec3d424a198c96
Milad Joodi <winbeta.com@gmail.com>
2024-02-02 01:12:11 +0330

    cont

--- 14.Todo-Fullstack/pages/api/auth/todos/index.js
+++ 14.Todo-Fullstack/pages/api/auth/todos/index.js
@@ -8,6 +8,19 @@ const handler = (res, req) => {
     }else if(req.method === 'POST'){
         //code=> Create new todo
         
+        connectToDB()
+
+        const { token } = req.cookies;
+      
+        if (!token) {
+          res.status(401).json({ message: "You are not login" })
+        }
+      
+        const tokenPayload = verifyToken(token);
+      
+        if (!tokenPayload) {
+          return res.status(401).json({ message: "You are not login" })
+        }
 
 
     }else{
