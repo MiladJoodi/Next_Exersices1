0a2a8dd137621206c85d2210a6a3f954fd2649a7
Milad Joodi <winbeta.com@gmail.com>
2024-02-01 17:50:52 +0330

    0

--- 12/pages/api/posts.js
+++ 12/pages/api/posts.js
@@ -3,29 +3,46 @@ const prisma = new PrismaClient();
 
 export default async function userServices(req, res) {
 
-    // if (req.method === 'POST') {
-    //     const { title, content, authorId, published } = req.body
-    //     const user = await prisma.user.create({
-    //         data: {
-    //             email: "sss@gmail.com",
-    //             name: "miald",
-    //             password: "123456"
-    //         }
-    //     })
-    //     return res.json(user)
-    // }
-
+  //POST
     if (req.method === 'POST') {
         const {title, content, authorId, published} = req.body;
+        console.log("first")
         const post = await prisma.post.create({
           data: {
             title,
             content,
             authorId,
-            published,
+            published
           },
         });
         return res.json(post);
       }
+  //POST
+
+  // GET
+    if(req.method === 'GET'){
+      const {id} = req.query;
+      if(id){
+        const post = await prisma.post.findFirst({
+        where:{
+          id : Number(id)
+        },
+        })
+        if(!post){
+          return res.json({message: "Not Found"})
+        }
+        res.json(post)
+      }else{
+        const posts = await prisma.post.findMany({
+          where:{
+            published: true
+          }
+        })
+        res.json(posts)
+      }
+      res.status = 400
+      res.json({error: "Bad Request"})
+    }
+  // GET
 
 }
