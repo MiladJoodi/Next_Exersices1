5d7596512e34c958589d73d429ac7e9d52e4f2c1
Milad Joodi <winbeta.com@gmail.com>
2024-02-03 20:20:43 +0330

    Next Auth

---
+++ 15.Next-Auth/pages/api/auth/[...nextauth].js
@@ -0,0 +1,41 @@
+import connectToDB from "@/configs/db";
+import UserModel from "@/models/User"
+import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export default NextAuth({
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    CredentialsProvider({
+      name: "name_here",
+      async authorize(credentials, req) {
+        connectToDB()
+        
+        const {identifier, password} = credentials
+
+        if(!identifier.trim() || !password.trim()){
+          throw new Error("Data in not valid")
+        }
+
+        //Find User
+        const user = await UserModel.findOne({
+          $or: [{username: identifier}, {email: identifier}]
+        })
+
+        if(!user){
+          throw new Error("User not found")
+        }
+
+        //Password Validation
+        const isValidPassword = await verifyPassword(password, user.password)
+        if(!isValidPassword){
+          throw new Error('Username Or Password Is Not Valid')
+        }
+
+        return {email: user.email}; // Jwt Payload
+      },
+    }),
+  ],
+});
---
+++ 15.Next-Auth/pages/signin/index.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { signIn } from "next-auth/react";
+
+function Index() {
+  const [identifier, setIdentifier] = useState("");
+  const [password, setPassword] = useState("");
+
+
+  const signin = async (event)=>{
+    event.preventDefault()
+    const res = await signIn("credentials", {
+      identifier,
+      password,
+      redirect:false
+    })
+  }
+
+  return (
+    <div className="box">
+      <h1 align="center">Login Form</h1>
+      <form role="form" method="post">
+        <div className="inputBox">
+          <input
+            type="text"
+            autoComplete="off"
+            value={identifier}
+            onChange={(event) => setIdentifier(event.target.value)}
+            required
+          />
+          <label>Username | Email</label>
+        </div>
+        <div className="inputBox">
+          <input
+            type="password"
+            autoComplete="off"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            required
+          />
+          <label>Password</label>
+        </div>
+
+        <input
+          type="submit"
+          className="register-btn"
+          value="Sign In"
+          onClick={signin}
+        />
+      </form>
+    </div>
+  );
+}
+
+export default Index;
