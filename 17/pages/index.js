f6e3788a3d8bf9720b426c06681d8500fd10b086
Milad Joodi <winbeta.com@gmail.com>
2024-02-06 00:36:03 +0330

    0

--- 17/pages/index.js
+++ 17/pages/index.js
@@ -1,31 +1,13 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Link from 'next/link'
-import {
-  CredentialsSignInButton,
-  GithubSignInButton,
-  GoogleSignInButton,
-} from "@/components/authButtons";
+import styles from "@/styles/Home.module.css";
+import { signIn } from "next-auth/react";
 
-const  home = async () => {
-  const session = await getServerSession(authConfig);
 
-  console.log("Session: ", session);
-
-  if (session) return redirect("/timeline");
-
+export default function Home() {
   return (
-    <div className="w-full flex flex-col items-center justify-center min-h-screen py-2">
-      <div className="flex flex-col items-center mt-10 p-10 shadow-md">
-        <h1 className="mt-10 mb-4 text-4xl font-bold">Sign In</h1>
-        <GoogleSignInButton />
-        <GithubSignInButton />
-        <span className="text-2xl font-semibold text-white text-center mt-8">
-          Or
-        </span>
-        {/* <CredentialsSignInButton /> */}
-        <CredentialsForm />
-      </div>
-    </div>
+      <main className={`${styles.main}`}>
+         <button type="button"
+                className="btn btn-link btn-floating-mx-1"
+                onClick={() => signIn("github")}>Login with Github</button>
+      </main>
   );
 }
