67384b77d0cb8405553fa1f78d0446a8f6aed776
Milad Joodi <winbeta.com@gmail.com>
2024-02-09 01:52:49 +0330

    0

--- film/app/page.jsx
+++ film/app/page.jsx
@@ -12,13 +12,21 @@ const Home = async () => {
             <div className="text-center text-2xl text-blue-500">صفحه اصلی</div>
             <div className="flex justify-between items-center flex-wrap">
                 {data.data.map((mov, i) => (
-                    <div className="h-[30rem] my-4 w-72 border-2 border-zinc-300 rounded-2xl" key={i}>
-                        <Image 
-                        width={260} 
-                        height={300} 
-                        alt={mov.title} 
-                        src={mov.poster} 
-                        />
+                    <div className="relative h-[28rem] my-4 w-72 border-2 border-zinc-300 rounded-2xl p-4 flex flex-col gap-4" key={i}>
+                        <div className="flex justify-center items-center">
+                            <Image
+                                className="rounded-lg"
+                                width={220}
+                                height={260}
+                                alt={mov.title}
+                                src={mov.poster}
+                            />
+                        </div>
+                        <h3>{mov.title}</h3>
+                        <div className="absolute bottom-2 right-2 left-2 flex justify-between items-center">
+                            <div className="px-3 py-1 bg-zinc-200 rounded-md">{mov.year}</div>
+                            <div className="px-3 py-1 bg-zinc-200 rounded-md">{mov.country}</div>
+                        </div>
                     </div>
                 ))}
             </div>
