36ee3ec0db29bd452195c3a1e52482042a8c13a6
Milad Joodi <winbeta.com@gmail.com>
2024-02-08 22:48:49 +0330

    0

--- film/app/layout.jsx
+++ film/app/layout.jsx
@@ -1,10 +1,14 @@
+import Header from "@/components/header";
 import "../styles/globals.css"
+import Footer from "@/components/footer";
 
 const RootLayout = ({ children }) => {
     return (
         <html lang="fa-IR">
             <body>
+                <Header />
                 {children}
+                <Footer />
             </body>
         </html>
     )
---
+++ film/components/footer/index.jsx
@@ -0,0 +1,8 @@
+const Footer = ()=>{
+    return(
+        <div className="center bg-zinc-500 h-[20vh] p-2 text-white flex justify-center items-center">
+            طراحی شده با  💜 
+        </div>
+    )
+}
+export default Footer
