fa7c4429935ac2b7e2d3a20439bb0a176bad5204
Milad Joodi <winbeta.com@gmail.com>
2024-02-10 00:30:53 +0330

    {/*⚠️ Brand Logo */}

--- 22.File-Shop/components/header/index.jsx
+++ 22.File-Shop/components/header/index.jsx
@@ -8,6 +8,7 @@ const Header = () => {
         <header className="container mx-auto py-2">
             <div className="flex justify-between items-center">
                 <div className='flex flex-col'>
+                    {/*⚠️ Brand Logo */}
                     <Link href={"/"}>
                         <div className="logo w-full h-full p-4 rounded-lg text-center shadow-[0px_1px_10px_rgba(0,0,0,0.25)] transition-all duration-500 hover:shadow-[0px_1px_10px_rgba(0,0,0,0.5)] ">
                             <Image
@@ -20,11 +21,23 @@ const Header = () => {
                             <div>فروشگاه فایل مرن فا</div>
                         </div>
                     </Link>
-                    <div className='flex justify-around  items-center text-[1.5rem] p-1 bg-red-300 rounded-br-md rounded-bl-md'>
-                        <BsTelegram />
-                        <AiFillTwitterCircle />
-                        <AiOutlineYoutube />
+                    {/*⚠️ Brand Logo */}
+
+
+                    {/*⚠️ Social Mediab Links  */}
+                    <div className='flex justify-around  items-center text-[1.5rem] p-2 bg-red-300 rounded-br-md rounded-bl-md'>
+                        <Link href="#">
+                            <BsTelegram />
+                        </Link>
+                        <Link href="#">
+                            <AiFillTwitterCircle />
+                        </Link>
+                        <Link href="#">
+                            <AiOutlineYoutube />
+                        </Link>
                     </div>
+                    {/*⚠️ Social Mediab Links  */}
+
                 </div>
                 <div>h</div>
             </div>
