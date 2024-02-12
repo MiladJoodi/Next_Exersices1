aaab16becba3ff7a3f39fb28577a86f5a25939fe
Milad Joodi <winbeta.com@gmail.com>
2024-02-12 04:05:49 +0330

    ♥

--- 24.Express js/controllers/PostsCtrl.js
+++ 24.Express js/controllers/PostsCtrl.js
@@ -2,7 +2,7 @@ const Post = require("../models/Post")
 
 const getAllPosts = async (req, res) => {
     try {
-        const allPosts = await Post.find().select({viewNum:1, data:1})
+        const allPosts = await Post.find().select({ viewNum: 1, data: 1 }).sort({ viewNum: 1 })
         allPosts.reverse()
         res.status(200).json(allPosts)
     } catch (e) {
@@ -28,7 +28,7 @@ module.exports.createPost = createPost
 const getOnePost = async (req, res) => {
     try {
         // const thePost = await Post.findById(req.params.id);
-        const thePost = await Post.findOne({_id: req.params.id});
+        const thePost = await Post.findOne({ _id: req.params.id });
         res.status(200).json(thePost)
     } catch (e) {
         res.status(400).json({ mgs: "خطا" })
@@ -40,8 +40,8 @@ module.exports.getOnePost = getOnePost
 const updatePost = async (req, res) => {
     try {
         const newData = req.body;
-        await Post.findByIdAndUpdate(req.params.id, newData, {new: true});
-        res.status(200).json({msg: "post updated"})
+        await Post.findByIdAndUpdate(req.params.id, newData, { new: true });
+        res.status(200).json({ msg: "post updated" })
     } catch (e) {
         res.status(400).json({ mgs: "خطا" })
     }
@@ -51,8 +51,8 @@ module.exports.updatePost = updatePost
 //Delete
 const removePost = async (req, res) => {
     try {
-        await Post.deleteOne({_id: req.params.id});
-        res.status(200).json({msg: "post deleted"})
+        await Post.deleteOne({ _id: req.params.id });
+        res.status(200).json({ msg: "post deleted" })
     } catch (e) {
         res.status(400).json({ mgs: "خطا" })
     }
