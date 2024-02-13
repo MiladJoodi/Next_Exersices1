a1e4a4680eb49a9f1b593807f716b587132791d5
Milad Joodi <winbeta.com@gmail.com>
2024-02-13 23:08:14 +0330

    0

--- 22.File-Shop/components/graphic-cats/index.jsx
+++ 22.File-Shop/components/graphic-cats/index.jsx
@@ -2,6 +2,8 @@ import CatBox from "./Box"
 
 const GraphicCategories = () => {
     return (
+        // Grid test *
+        // <section className=" container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
         <section className=" container mx-auto flex justify-center sm:justify-between items-center flex-wrap gap-2">
             <CatBox />
             <CatBox />
--- 22.File-Shop/components/newBlogs/index.jsx
+++ 22.File-Shop/components/newBlogs/index.jsx
@@ -2,11 +2,19 @@ import BlogBox from "./BlogBox";
 
 const NewBlogs = () => {
     return (
-        <section className="container flex justify-center items-center flex-wrap gap-2">
-            <BlogBox/>
-            <BlogBox/>
-            <BlogBox/>
-            <BlogBox/>
+        <section className="flex flex-col gap-[1.5rem] container mx-auto">
+            <header className=" flex justify-between items-center">
+                  <h2 className=" text-2xl border-r-zinc-700 border-r-2 pr-1">
+                     آخرین مقالات
+                  </h2>
+               </header>
+            <div className="container flex justify-center items-center flex-wrap gap-2">
+                <BlogBox />
+                <BlogBox />
+                <BlogBox />
+                <BlogBox />
+            </div>
+
         </section>
     );
 }
