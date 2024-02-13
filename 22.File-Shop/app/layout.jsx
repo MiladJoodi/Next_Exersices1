1856f2b53094ec040976a667ca128fccf1576782
Milad Joodi <winbeta.com@gmail.com>
2024-02-14 01:57:23 +0330

    0

--- 22.File-Shop/app/layout.jsx
+++ 22.File-Shop/app/layout.jsx
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
       <html lang="fa-IR">
          <body>
             <Header />
-            {/* {children} */}
+            {children}
             <Footer />
          </body>
       </html>
