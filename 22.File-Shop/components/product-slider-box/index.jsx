7c26399690feaa5aa6b189bb8627b01f67df8d8c
Milad Joodi <winbeta.com@gmail.com>
2024-02-13 17:00:44 +0330

    up

--- 22.File-Shop/components/product-slider-box/index.jsx
+++ 22.File-Shop/components/product-slider-box/index.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import { HiShoppingBag } from "react-icons/hi";
 import { AiOutlineSearch } from "react-icons/ai";
+import { HiOutlineShoppingCart } from "react-icons/hi";
 import Link from "next/link";
 
 
@@ -32,16 +32,22 @@ const SlideBox = () => {
                      </div>
                   </div>
 
-                  <div className=" absolute bottom-2  w-full flex justify-between items-center">
-                     <div className="bg-zinc-200 flex justify-center items-center w-9 h-9 rounded-lg transition-all duration-500 hover:bg-zinc-300 cursor-pointer">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 font-bold">
-                           <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
-                        </svg>
-
+                  <div className=" absolute bottom-2 w-full flex justify-between items-center">
+                     <div className="flex gap-2 justify-start items-center mr-1">
+                        <div className="bg-zinc-200 flex justify-center items-center w-9 h-9 rounded-lg transition-all duration-500 hover:bg-zinc-300 cursor-pointer">
+                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 font-bold">
+                              <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
+                           </svg>
+                        </div>
+                        <div className="bg-zinc-200 flex justify-center items-center w-9 h-9 rounded-lg transition-all duration-500 hover:bg-zinc-300 cursor-pointer">
+                           <AiOutlineSearch />
+                        </div>
                      </div>
-                     <HiShoppingBag className=" mr-1 w-10 h-10 p-2 rounded bg-zinc-200 text-indigo-600  cursor-pointer transition-all duration-300 hover:bg-orange-500 hover:text-white" />
-                     <div className=" bg-zinc-500 text-white p-2 rounded-tr-md rounded-br-md">
-                        5000 تومان
+                     <div className="flex gap-2 justify-end items-center">
+                        <HiOutlineShoppingCart className=" mr-1 w-9 h-9 p-2 rounded bg-zinc-200 text-indigo-600  cursor-pointer transition-all duration-300 hover:bg-orange-500 hover:text-white" />
+                        <div className=" bg-zinc-500 text-white h-9 px-1 flex justify-center items-center rounded-tr-md rounded-br-md">
+                           5000 تومان
+                        </div>
                      </div>
                   </div>
                </div>
