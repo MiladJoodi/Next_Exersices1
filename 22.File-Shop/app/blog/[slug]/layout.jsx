2534d39357ad70cf3d080d07d1fbf1ef962d69f1
Milad Joodi <winbeta.com@gmail.com>
2024-02-14 23:58:16 +0330

    up

--- 22.File-Shop/app/blog/[slug]/layout.jsx
+++ 22.File-Shop/app/blog/[slug]/layout.jsx
@@ -1,10 +1,14 @@
+import { BiSearchAlt } from "react-icons/bi";
 
 const BlogLayout = ({ children }) => {
     return (
-        <div className="flex justify-between items-start container mx-auto">
-            <main className="w-full">{children}</main>
-            <aside className="w-72 max-w-72 p-4 rounded-md bg-zinc-100">
-                1
+        <div className="flex justify-between items-start container mx-auto gap-2">
+            <main className="w-[75%]">{children}</main>
+            <aside className="w-80 max-w-80 p-2 rounded-md bg-zinc-100">
+                <form className="border-zinc-700 border-2 px-2 rounded-md flex justify-between items-center">
+                    <input type="text" className="bg-transparent p-2 outline-none text-sm" placeholder="جستجو در بلاگ" />
+                    <BiSearchAlt className="w-8 h-8" />
+                </form>
             </aside>
         </div>
     );
