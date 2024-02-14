e34e6969d6940e665e8b9779ea0d5df05a94d2a3
Milad Joodi <winbeta.com@gmail.com>
2024-02-14 23:10:12 +0330

    ♣

--- 22.File-Shop/components/newBlogs/BlogBox.jsx
+++ 22.File-Shop/components/newBlogs/BlogBox.jsx
@@ -5,7 +5,7 @@ const BlogBox = () => {
     return (
         <article className="sliderItem p-2 transition-all duration-300 hover:pt-0">
 
-            <div className="relative bg-white shadow-[0px_1px_8px_rgba(0,0,0,.2)] h-[28rem] w-72 rounded-lg">
+            <div className="relative bg-white shadow-[0px_1px_8px_rgba(0,0,0,.2)] h-[26rem] w-72 rounded-lg">
 
                 <Link href={"/shop"} target={"_blank"}>
                     <div className="flex justify-center items-center pt-2">
@@ -22,7 +22,7 @@ const BlogBox = () => {
                 <div>
                     <div className="flex flex-col gap-6 p-2">
                         <Link href={"/shop"}>
-                            <h3 className="line-clamp-3">
+                            <h3 className="line-clamp-2">
                                 عنوان محصول هستن و ان محصول هستن وان محصول هستن وان محصول
                                 هستن وانهستن وان محصول هستن وان محصول
                                 هستن وانهستن وان محصول هستن وان محصول
@@ -33,11 +33,11 @@ const BlogBox = () => {
                             </h3>
                         </Link>
 
-                        <p className="text-base sm:text-sm text-justify line-clamp-3">
+                        <p className="text-base sm:text-sm text-justify line-clamp-4">
                             لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستونبلکه روزنامه و مجله در ستونبلکه روزنامه و مجله در ستونبلکه بلکه روزنامه و مجله در ستونبلکه روزنامه و مجله در ستونروزنامه و مجله در ستونبلکه روزنامه و مجله بلکه روزنامه و مجله در ستون که لازم است
                         </p>
                         <div className="h-1 w-[90%] bg-zinc-300 border-2 rounded-full mx-auto"/>
-                        <div className="flex justify-between items-center">
+                        <div className="absolute bottom-2 right-2 left-2 flex justify-between items-center">
                             <div className="text-base sm:text-sm bg-zinc-300 rounded-md py-1 px-3">1402/04/15</div>
                             <div className="text-base sm:text-sm bg-zinc-300 rounded-md py-1 px-3">5 دیدگاه</div>
                         </div>
--- 22.File-Shop/components/sliders/realatedPosts/index.jsx
+++ 22.File-Shop/components/sliders/realatedPosts/index.jsx
@@ -5,7 +5,7 @@ import { FaChevronRight } from "react-icons/fa";
 import { FaChevronLeft } from "react-icons/fa";
 import { useRef } from "react";
 
-const ProductsSlider = ({ title, linkCom }) => {
+const RelatedPosts = () => {
 
    const carouselRef = useRef();
    const carouselSwitcher = (data) => {
@@ -18,10 +18,11 @@ const ProductsSlider = ({ title, linkCom }) => {
    }
 
    return (
-      <div className=" bg-indigo-500">
+      <div className="">
          <div className="container mx-auto py-8">
-            <section className="flex flex-col gap-4 px-2">
+            <section className="flex flex-col gap-6 px-2">
                <header className=" flex justify-between items-center">
+                <h2 className="text-xl">مطالب مرتبط</h2>
                   <div className="flex gap-1">
                      <div className=" flex items-center gap-1 text-zinc-500">
                         <FaChevronRight onClick={() => { carouselSwitcher(1) }} className=" cursor-pointer bg-zinc-200 transition-all duration-300 hover:text-white hover:bg-orange-400 w-10 h-10 p-3 rounded" />
@@ -46,4 +47,4 @@ const ProductsSlider = ({ title, linkCom }) => {
    );
 };
 
-export default ProductsSlider;
+export default RelatedPosts;
