64c8e0d068b00c967ba2a0a32daf5f3aabc22cf4
Milad Joodi <winbeta.com@gmail.com>
2024-02-15 23:18:40 +0330

    ♥

--- 22.File-Shop/app/loading.jsx
+++ 22.File-Shop/app/loading.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Loading = ()=>{
     return(
         <div className="flex justify-center items-center p-12">
-            <Image width={100} height={100} alt="loading" src={"/loading.svg"} />
+            <Image width={100} height={100} alt="loading" src={"/images/loading.svg"} />
         </div>
     );
 }
