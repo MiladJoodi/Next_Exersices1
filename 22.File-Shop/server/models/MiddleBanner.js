597aae43adfcdec362b697df3e0253c11911769c
Milad Joodi <winbeta.com@gmail.com>
2024-02-16 21:45:45 +0330

    update

--- 22.File-Shop/server/models/MiddleBanner.js
+++ 22.File-Shop/server/models/MiddleBanner.js
@@ -1,24 +1,27 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const MiddleBanner = new mongoose.Schema({
-    image:{
-        required: true,
-        type: String
-    },
-    imageAlt:{
-        required: true,
-        type: String
-    },
-    situation:{
-        required: true,
-        type: Boolean
-    },
-    link:{
-        required: true,
-        type: String
-    },
-    date: {
-        type: String,
-        default: new Date().toLocaleDateString('fa-IR', { hour: '2-digit', minute: '2-digit' })
-           },
-})
+  image: {
+    required: true,
+    type: String,
+  },
+  imageAlt: {
+    required: true,
+    type: String,
+  },
+  situation: {
+    required: true,
+    type: Boolean,
+  },
+  link: {
+    required: true,
+    type: String,
+  },
+  date: {
+    type: String,
+    default: new Date().toLocaleDateString("fa-IR", {
+      hour: "2-digit",
+      minute: "2-digit",
+    }),
+  },
+});
 module.exports = mongoose.model("MiddleBanner", MiddleBanner);
