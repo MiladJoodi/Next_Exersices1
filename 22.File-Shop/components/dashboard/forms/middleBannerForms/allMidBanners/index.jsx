ba55ae4990767483a73179f8bc59cdbb2aafa952
Milad Joodi <winbeta.com@gmail.com>
2024-02-17 22:27:38 +0330

    pagination

--- 22.File-Shop/components/dashboard/forms/middleBannerForms/allMidBanners/index.jsx
+++ 22.File-Shop/components/dashboard/forms/middleBannerForms/allMidBanners/index.jsx
@@ -1,5 +1,15 @@
+"use client"
+import { useState, useEffect } from 'react'
+import axios from 'axios';
 
 const AllMidBanners = ()=>{
+    const [banners, setbanners] = useState([])
+    const [pageNumber, setPageNumber] = useState(1)
+    useEffect(()=>{
+        axios.get(`http://localhost:27017/api/middle-banners?pn=${pageNumber}`)
+        .then(data=> setbanners(data))
+        .catch(e=>console.log(e))
+    },[])
     return(
         <div>
             AllMidBanners
