d59b9b6bf381c47bbe0af7d989e3aedb695a18e6
Milad Joodi <winbeta.com@gmail.com>
2024-02-17 23:44:01 +0330

    ♥

--- 22.File-Shop/components/dashboard/forms/middleBannerForms/allMidBanners/index.jsx
+++ 22.File-Shop/components/dashboard/forms/middleBannerForms/allMidBanners/index.jsx
@@ -1,20 +1,36 @@
-"use client"
-import { useState, useEffect } from 'react'
-import axios from 'axios';
+"use client";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Box from "./Box";
+import Image from "next/image";
 
-const AllMidBanners = ()=>{
-    const [banners, setbanners] = useState([])
-    const [pageNumber, setPageNumber] = useState(1)
-    useEffect(()=>{
-        axios.get(`http://localhost:27017/api/middle-banners?pn=${pageNumber}`)
-        .then(data=> setbanners(data))
-        .catch(e=>console.log(e))
-    },[])
-    return(
-        <div>
-            AllMidBanners
-        </div>
-    );
-}
+const AllMidBanners = () => {
+  const [banners, setbanners] = useState([]);
+  const [pageNumber, setPageNumber] = useState(1);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:27017/api/middle-banners?pn=${pageNumber}`)
+      .then((d) => setbanners(d.data))
+      .catch((e) => console.log(e));
+  }, [banners]);
+  return (
+    <div className="p-4 flex flex-col gap-8">
+      <div className="flex flex-col gap-6">
+        {banners.length < 1 ? (
+          <div className="flex justify-center items-center p-12">
+            <Image
+              width={100}
+              height={100}
+              alt="loading"
+              src={"/images/loading.svg"}
+            />
+          </div>
+        ) : (
+          banners.map((ba, i) => <Box key={i} data={ba} />)
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default AllMidBanners;
