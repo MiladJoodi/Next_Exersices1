f68f6252e1ba92eb5b1a8d9b75257773a04d88d8
Milad Joodi <winbeta.com@gmail.com>
2024-08-28 20:34:31 +0330

    up

---
+++ 25.searchParams/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  let name = "milad";
+  let counter: number = 1
+  let flag: boolean = false
+  let array :number[] = [1,2,3,4]
+
+  return <div>Home</div>;
+}
